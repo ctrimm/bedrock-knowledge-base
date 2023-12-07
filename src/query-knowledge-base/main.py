e930cb182ccb85cc132f887b9200e87c8d59ec11
Jose <jose.andresmr@hotmail.com>
2023-12-07 16:34:56 -0600

    Add sync kb Lambda

--- src/query-knowledge-base/main.py
+++ src/query-knowledge-base/main.py
@@ -3,21 +3,22 @@ import json
 
 
 def lambda_handler(event, context):
-    bedrock_agent_runtime = boto3.client(
+    bedrock_client = boto3.client(
         service_name="bedrock-agent-runtime",
         region_name="us-east-1",
     )
 
     input = event.get('input')
+    knowledge_base_id = event.get('knowledgeBaseId')
 
-    response = bedrock_agent_runtime.retrieve_and_generate(
+    response = bedrock_client.retrieve_and_generate(
         input={
             'text': input
         },
         retrieveAndGenerateConfiguration={
             'type': 'KNOWLEDGE_BASE',
             'knowledgeBaseConfiguration': {
-                'knowledgeBaseId': 'your_knowledge_base_id',
+                'knowledgeBaseId': knowledge_base_id,
                 'modelArn': 'arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2'
             }
         }
---
+++ src/sync-knowledge-base/main.py
@@ -0,0 +1,29 @@
+import boto3
+
+
+def lambda_handler(event, context):
+    knowledge_base_id = event.get('knowledgeBaseId')
+    data_source_id = event.get('dataSourceId')
+
+    bedrock_client = boto3.client(
+        service_name="bedrock-agent",
+        region_name="us-east-1",
+    )
+
+    response = bedrock_client.start_ingestion_job(
+        knowledgeBaseId=knowledge_base_id,
+        dataSourceId=data_source_id,
+    )
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+        },
+        "body": {
+            "ingestionJobId": response["ingestionJob"]["ingestionJobId"]
+        },
+    }
