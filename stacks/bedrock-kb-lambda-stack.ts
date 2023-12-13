7e4504136d4b32ef23501e9f3189285bd5f712ce
Jose <jose.andresmr@hotmail.com>
2023-12-13 15:04:21 -0600

    Create custom IAM Role

--- stacks/bedrock-kb-lambda-stack.ts
+++ stacks/bedrock-kb-lambda-stack.ts
@@ -7,37 +7,97 @@ function getPyBundlePath(name: string) {
 }
 
 export function BedrockKbLambdaStack({ stack }: StackContext) {
-  const kbDocumentsBucket = new Bucket(stack, "kb-documents");
+  const kbDocumentsBucket = new Bucket(stack, "bedrock-kb-documents");
 
-  const bedrockKb = new BedrockKnowledgeBase(
-    stack,
-    "BedrockOpenSearchKnowledgeBase",
-    {
-      name: `bedrock-kb-${stack.stage}`,
-      roleArn:
-        "arn:aws:iam::637732166235:role/service-role/AmazonBedrockExecutionRoleForKnowledgeBase_7yi0z",
-      storageConfiguration: {
-        pineconeConfiguration: {
-          connectionString:
-            "https://bedrock-kb-3xrbu48.svc.gcp-starter.pinecone.io",
-          credentialsSecretArn:
-            "arn:aws:secretsmanager:us-east-1:637732166235:secret:pinecon-api-ley-JTCk8V",
-          fieldMapping: {
-            metadataField: "metadata",
-            textField: "text",
-          },
+  const bedrockKbRole = new iam.Role(stack, "bedrock-kb-role", {
+    roleName: `AmazonBedrockExecutionRoleForKnowledgeBase_bkb-${stack.stage}`,
+    description: "IAM role to create a Bedrock Knowledge Base",
+    assumedBy: new iam.ServicePrincipal("bedrock.amazonaws.com", {
+      conditions: {
+        ["StringEquals"]: {
+          "aws:SourceAccount": stack.account,
         },
-        type: "PINECONE",
-      },
-      dataSource: {
-        dataSourceConfiguration: {
-          s3Configuration: {
-            bucketArn: kbDocumentsBucket.bucketArn,
-          },
+        ["ArnLike"]: {
+          "aws:SourceArn": `arn:aws:bedrock:us-east-1:${stack.account}:knowledge-base/*`,
         },
       },
-    }
-  );
+    }),
+    managedPolicies: [
+      new iam.ManagedPolicy(stack, "bedrock-kb-invoke", {
+        statements: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ["bedrock:InvokeModel"],
+            resources: [
+              "arn:aws:bedrock:us-east-1::foundation-model/amazon.titan-embed-text-v1",
+            ],
+          }),
+        ],
+      }),
+      new iam.ManagedPolicy(stack, "bedrock-kb-s3-managed-policy", {
+        statements: [
+          new iam.PolicyStatement({
+            sid: "S3ListBucketStatement",
+            effect: iam.Effect.ALLOW,
+            actions: ["s3:ListBucket"],
+            resources: [`${kbDocumentsBucket.bucketArn}/*`],
+            conditions: {
+              ["StringEquals"]: {
+                "aws:ResourceAccount": stack.account,
+              },
+            },
+          }),
+          new iam.PolicyStatement({
+            sid: "S3GetObjectStatement",
+            effect: iam.Effect.ALLOW,
+            actions: ["s3:GetObject"],
+            resources: [`${kbDocumentsBucket.bucketArn}/*`],
+            conditions: {
+              ["StringEquals"]: {
+                "aws:ResourceAccount": stack.account,
+              },
+            },
+          }),
+        ],
+      }),
+      new iam.ManagedPolicy(stack, "bedrock-kb-secret-manager", {
+        statements: [
+          new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            actions: ["secretsmanager:GetSecretValue"],
+            resources: [
+              "arn:aws:secretsmanager:us-east-1:637732166235:secret:pinecon-api-ley-JTCk8V",
+            ],
+          }),
+        ],
+      }),
+    ],
+  });
+
+  const bedrockKb = new BedrockKnowledgeBase(stack, "bedrock-knowledge-base", {
+    name: `bedrock-kb-${stack.stage}`,
+    roleArn: bedrockKbRole.roleArn,
+    storageConfiguration: {
+      pineconeConfiguration: {
+        connectionString:
+          "https://bedrock-kb-3xrbu48.svc.gcp-starter.pinecone.io",
+        credentialsSecretArn:
+          "arn:aws:secretsmanager:us-east-1:637732166235:secret:pinecon-api-ley-JTCk8V",
+        fieldMapping: {
+          metadataField: "metadata",
+          textField: "text",
+        },
+      },
+      type: "PINECONE",
+    },
+    dataSource: {
+      dataSourceConfiguration: {
+        s3Configuration: {
+          bucketArn: kbDocumentsBucket.bucketArn,
+        },
+      },
+    },
+  });
 
   const promptFunction = new Function(stack, "prompt", {
     runtime: "python3.12",
@@ -51,14 +111,13 @@ export function BedrockKbLambdaStack({ stack }: StackContext) {
   });
   promptFunction.addToRolePolicy(
     new iam.PolicyStatement({
-      sid: "StartIngestionJob",
       effect: iam.Effect.ALLOW,
       actions: [
         "bedrock:RetrieveAndGenerate",
         "bedrock:Retrieve",
         "bedrock:InvokeModel",
       ],
-      resources: ["*"],
+      resources: ["*"], // TODO: Use only the recently created knowledge base arn
     })
   );
 
@@ -75,7 +134,6 @@ export function BedrockKbLambdaStack({ stack }: StackContext) {
   });
   syncKnowledgeBaseFunction.addToRolePolicy(
     new iam.PolicyStatement({
-      sid: "StartIngestionJob",
       effect: iam.Effect.ALLOW,
       actions: [
         "bedrock:StartIngestionJob",
